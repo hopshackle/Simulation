d34e389b1a2852171a50c60702936401f641e334
hopshackle <james@janigo.co.uk>
2016-05-13 09:44:19 +0100

    Framework for AP tests

---
+++ hopshackle/src/hopshackle/simulation/test/refactor/ActionProcessorTests.java
@@ -0,0 +1,28 @@
+package hopshackle.simulation.test.refactor;
+
+import static org.junit.Assert.*;
+import hopshackle.simulation.*;
+import hopshackle.simulation.test.refactor.TestAgent;
+import hopshackle.simulation.test.refactor.TestActionFactory;
+
+import org.junit.Test;
+
+public class ActionProcessorTests {
+
+	@Test
+	public void addingAnActionQueuesItForPlannedStartTime() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void afterActionHasBeenStartedItIsQueuedForEndTime() {
+		fail("Not yet implemented.");
+	}
+	@Test
+	public void afterActionHasBeenExecutedANewOneIsQueuedForStartTimeIfAgentPlanIsEmpty() {
+		fail("Not yet implemented.");
+	}
+	@Test
+	public void ifAgentQueueIsFullThenNoNewDecisionIsTaken() {
+		fail("Not yet implemented.");
+	}
+}
