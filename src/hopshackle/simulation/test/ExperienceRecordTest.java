409d3d5a205856c716fdf959985273bedd6dcec2
hopshackle <james@janigo.co.uk>
2016-08-20 13:00:19 +0100

    Added monteCarlo reward
    MonteCarloReward=true to activate
    This then takes the final reward and back-tracks it through all
    predecing ExperienceRecords (applying gamma as a discount factor).
    Interim rewards are added in and back-tracked too.

--- src/hopshackle/simulation/test/ExperienceRecordTest.java
+++ src/hopshackle/simulation/test/ExperienceRecordTest.java
@@ -17,6 +17,8 @@ public class ExperienceRecordTest {
 
 	@Before
 	public void setUp() throws Exception {
+		SimProperties.setProperty("IncrementalScoreReward", "true");
+		ExperienceRecord.refreshProperties();
 		for (GeneticVariable<BasicAgent> gv : BasicVariables.values())
 			varList.add(gv);
 		w = new World();
@@ -51,6 +53,65 @@ public class ExperienceRecordTest {
 		assertTrue(er.getActionTaken().getType() == BasicActions.FARM);
 		assertEquals(er.getReward(), 15.0, 0.01);	// -15 in change of score from full health, plus 30 reward
 		assertEquals(er.getPossibleActionsFromStartState().size(), 2);
+	}
+	
+	@Test
+	public void monteCarloRewardsWithIncremental() {
+		String oldGamma = SimProperties.getProperty("Gamma", "0.9");
+		SimProperties.setProperty("MonteCarloReward", "true");
+		SimProperties.setProperty("Gamma", "0.9");
+		ExperienceRecord.refreshProperties();
+		
+		List<ActionEnum<BasicAgent>> possibleActions = new ArrayList<ActionEnum<BasicAgent>>();
+		possibleActions.add(BasicActions.FARM);
+		possibleActions.add(BasicActions.FIND_UNKNOWN);
+
+		er = new ExperienceRecord<BasicAgent>(agent, new LinearState<BasicAgent>(agent, varList), BasicActions.FARM.getAction(agent), possibleActions);
+		er.updateWithResults(30.0, new LinearState<BasicAgent>(agent, varList));
+		ExperienceRecord<BasicAgent> nextER = new ExperienceRecord<BasicAgent>(agent, new LinearState<BasicAgent>(agent, varList), BasicActions.FARM.getAction(agent), possibleActions);
+		er.updateNextActions(nextER);
+		nextER.updateWithResults(0.0, new LinearState<BasicAgent>(agent, varList));
+		assertEquals(er.getReward(), 30.0, 0.001);
+		assertEquals(nextER.getReward(), 0.0, 0.001);
+		agent.addHealth(-2.0);
+		nextER.setIsFinal();
+		assertEquals(nextER.getReward(), -2.0, 0.001);
+		assertEquals(er.getReward(), -2.0 * 0.9 + 30.0, 0.001);
+
+		
+		SimProperties.setProperty("MonteCarloReward", "false");
+		SimProperties.setProperty("Gamma", oldGamma);
+		ExperienceRecord.refreshProperties();
+	}
+	
+	@Test
+	public void monteCarloRewardsWithoutIncremental() {
+		String oldGamma = SimProperties.getProperty("Gamma", "0.9");
+		SimProperties.setProperty("MonteCarloReward", "true");
+		SimProperties.setProperty("IncrementalScoreReward", "false");
+		SimProperties.setProperty("Gamma", "0.9");
+		ExperienceRecord.refreshProperties();
+		
+		List<ActionEnum<BasicAgent>> possibleActions = new ArrayList<ActionEnum<BasicAgent>>();
+		possibleActions.add(BasicActions.FARM);
+		possibleActions.add(BasicActions.FIND_UNKNOWN);
+
+		er = new ExperienceRecord<BasicAgent>(agent, new LinearState<BasicAgent>(agent, varList), BasicActions.FARM.getAction(agent), possibleActions);
+		er.updateWithResults(3.0, new LinearState<BasicAgent>(agent, varList));
+		ExperienceRecord<BasicAgent> nextER = new ExperienceRecord<BasicAgent>(agent, new LinearState<BasicAgent>(agent, varList), BasicActions.FARM.getAction(agent), possibleActions);
+		er.updateNextActions(nextER);
+		nextER.updateWithResults(0.0, new LinearState<BasicAgent>(agent, varList));
+		assertEquals(er.getReward(), 3.0, 0.001);
+		assertEquals(nextER.getReward(), 0.0, 0.001);
+		agent.addHealth(-2.0);
+		nextER.setIsFinal();
+		assertEquals(nextER.getReward(), 18.0, 0.001);
+		assertEquals(er.getReward(), 18.0 * 0.9 + 3.0, 0.001);
+
+		
+		SimProperties.setProperty("MonteCarloReward", "false");
+		SimProperties.setProperty("Gamma", oldGamma);
+		ExperienceRecord.refreshProperties();
 	}
 
 }
