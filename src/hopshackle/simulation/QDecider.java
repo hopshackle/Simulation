6b9431521d95a0679e6de7492027eb427b1ee9d5
hopshackle <james@janigo.co.uk>
2016-08-22 18:15:14 +0100

    Added monteCarlo flag to QDecider
    This was already being used in ExperienceRecords so that the final
    reward is backed up through all preceding ER; but QDecider still
    calculated a value of the next action.

--- src/hopshackle/simulation/QDecider.java
+++ src/hopshackle/simulation/QDecider.java
@@ -3,15 +3,18 @@ package hopshackle.simulation;
 import java.util.List;
 
 public abstract class QDecider<A extends Agent> extends BaseDecider<A> {
+	
+	private boolean monteCarlo;
 
 	public <S extends State<A>> QDecider(StateFactory<A> stateFactory, List<? extends ActionEnum<A>> actions) {
 		super(stateFactory, actions);
+		monteCarlo = SimProperties.getProperty("MonteCarloReward", "false").equals("true");
 	}
 
 	public abstract double valueOption(ActionEnum<A> option, State<A> state);
 
 	protected <S extends State<A>> double valueOfBestAction(ExperienceRecord<A> exp) {
-		if (exp.isInFinalState()) 
+		if (exp.isInFinalState() || monteCarlo) 
 			return 0.0;
 		ActionEnum<A> bestAction = getBestActionFrom(exp.getPossibleActionsFromEndState(), exp.getEndState());
 		return valueOption(bestAction, exp.getEndState());
